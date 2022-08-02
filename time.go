583f4c0ed4fbb8e80bc5627e094c0d30f0707133
a little <little@autre.cn>
2022-08-02 22:30:01 +0800

    增加bson time转换

--- time.go
+++ time.go
@@ -4,6 +4,9 @@ import (
 	"database/sql/driver"
 	"fmt"
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/bsontype"
+	"go.mongodb.org/mongo-driver/x/bsonx/bsoncore"
 )
 
 type Time struct {
@@ -56,4 +59,31 @@ func (t *Time) Scan(v interface{}) error {
 		return nil
 	}
 	return fmt.Errorf("can not convert %v to timestamp", v)
+}
+
+//实现 bson 的 序列化方法
+func (t *Time) MarshalBSONValue() (bsontype.Type, []byte, error) {
+	timestampt := t.Time.Format(timeFormart)
+	retByte := make([]byte, 0)
+	retByte = bsoncore.AppendString(retByte, timestampt)
+	return bsontype.String, retByte, nil
+}
+
+//实现 bson 的 反序列化方法
+func (t *Time) UnmarshalBSONValue(ty bsontype.Type, data []byte) error {
+	if ty == bsontype.String {
+		if readString, _, ok := bsoncore.ReadString(data); ok {
+			timeStr := string(readString)
+			if timeStr == "\"\"" {
+				*t = Time{}
+				return nil
+			}
+			now, err := time.ParseInLocation("\""+timeFormart+"\"", timeStr, time.Local)
+			if err != nil {
+				return err
+			}
+			*t = Time{now}
+		}
+	}
+	return nil
 }
---
+++ time_test.go
@@ -0,0 +1,12 @@
+package gowk
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestXxx(t *testing.T) {
+	tff := time.Now()
+	fmt.Println(tff)
+}
