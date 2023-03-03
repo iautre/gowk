7859d29163d80cbda6231ab50d5cc1526090a91e
a little <little@autre.cn>
2023-03-03 22:46:06 +0800

    配置文件读取

--- conf/init.go
+++ conf/init.go
@@ -12,17 +12,19 @@ func init() {
 }
 
 func Init(name string) {
-	// 文件路径
-	// if !filepath.IsAbs(name) {
-	// 	e, _ := os.Executable()
-	// 	name = filepath.Join(filepath.Dir(e), name)
-	// }
 	cfgTmp, err := ini.InsensitiveLoad(name)
 	if err != nil {
 		fmt.Printf("Fail to read file: %v", err)
 		os.Exit(1)
 	}
 	cfg = cfgTmp
+	Server = &ServerConf{}
+	if cfg.HasSection("server") {
+		cfg.Section("server").MapTo(Server)
+	} else {
+		fmt.Printf("no conf server, use default Addr: 80")
+		Server.Addr = ":80"
+	}
 	if cfg.HasSection("mysql") {
 		Mysql = &MysqlConf{}
 		cfg.Section("mysql").MapTo(Mysql)
@@ -32,10 +34,7 @@ func Init(name string) {
 		Mongo = &MongoConf{}
 		cfg.Section("mongo").MapTo(Mongo)
 	}
-	if cfg.HasSection("server") {
-		Server = &ServerConf{}
-		cfg.Section("server").MapTo(Server)
-	}
+
 	//组合全部
 	confs = make(conf)
 	for _, se := range cfg.Sections() {
