3073d3bdaff40cad6ae47f67794638e4d74b4f4e
a little <little@autre.cn>
2023-03-17 23:17:29 +0800

    log

--- log/log.go
+++ log/log.go
@@ -8,6 +8,7 @@ import (
 
 func Error(ctx context.Context, msg string, err error, arr ...any) {
 	arr = append(arr, getTraceId(ctx)...)
+	arr = append(arr, err.Error())
 	slog.Error(msg, arr...)
 }
 func Info(ctx context.Context, msg string, arr ...any) {
