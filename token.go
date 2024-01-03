a8dbc3c3ed08f81a4b9001454d362fc4149a5436
a little <little@autre.cn>
2024-01-03 20:22:40 +0800

    更新token

--- token.go
+++ token.go
@@ -7,19 +7,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-const CONTEXT_LOGIN_ID_KEY = "CONTEXT_LOGIN_ID_KEY"
-const TOKEN_KEY = "TOKEN_KEY"
+const CONTEXT_TOKEN_KEY = "ATOKEN_CONTEXT_TOKEN_KEY"
+const CONTEXT_TOKEN_VALUE_KEY = "ATOKEN_CONTEXT_TOKEN_VALUE_KEY"
+const CONTEXT_LOGIN_ID_KEY = "ATOKEN_CONTEXT_LOGIN_ID_KEY"
 
 var _defaultTokenHandler TokenHandler = &defaultToken{
 	Token: make(map[string]*Token),
 }
 var _defaultTokenName string = "atoken"
+var _defaultTokenTimeout int64 = 30 * 24 * 60 * 60 //默认为秒/-1为永久有效
 
 type Token struct {
 	Value   string `json:"value"`
 	Name    string `json:"name"`
 	Timeout int64  `json:"timeout"`
-	loginId any    `json:"-"`
+	LoginId any    `json:"loginId"`
+	Device  string `json:"device"`
 }
 
 func CheckLoginMiddleware() gin.HandlerFunc {
@@ -31,14 +34,14 @@ func CheckLogin(ctx *gin.Context) {
 	if tokenValue == "" {
 		Panic(ERR_TOKEN)
 	}
-	t, err := _defaultTokenHandler.GetToken(tokenValue)
+	token, err := _defaultTokenHandler.LoadToken(tokenValue)
 	if err != nil {
 		Panic(ERR_TOKEN)
 	}
-	if t == nil {
+	if token == nil {
 		Panic(ERR_TOKEN)
 	}
-	ctx.Set(CONTEXT_LOGIN_ID_KEY, t.loginId)
+	token.setContextToken(ctx, token)
 	ctx.Next()
 }
 
@@ -49,21 +52,38 @@ func SetTokenName(name string) {
 	_defaultTokenName = name
 }
 
+// 默认为秒
+func SetTokenTimeout(timeout int64) {
+	_defaultTokenTimeout = timeout
+}
+
 type longIdType interface {
 	string | int | uint | int64 | uint64
 }
 
-func Login[T longIdType](ctx *gin.Context, loginId T) *Token {
+func Login[T longIdType](ctx *gin.Context, loginId T) string {
 	token := &Token{
 		Value:   UUID(),
 		Name:    _defaultTokenName,
-		loginId: loginId,
+		Timeout: _defaultTokenTimeout,
+		LoginId: loginId,
 	}
-	_defaultTokenHandler.SaveToken(token.Value, token)
-	return token
+	token.setContextToken(ctx, token)
+	_defaultTokenHandler.StoreToken(token.Value, token)
+	return token.Value
 }
-
-func GetLoginId[T longIdType](ctx context.Context) T {
+func (t *Token) setContextToken(ctx *gin.Context, token *Token) {
+	ctx.Set(CONTEXT_TOKEN_KEY, token)
+	ctx.Set(CONTEXT_TOKEN_VALUE_KEY, token.Value)
+	ctx.Set(CONTEXT_LOGIN_ID_KEY, token.LoginId)
+}
+func TokenValue(ctx context.Context) string {
+	return ctx.Value(CONTEXT_TOKEN_VALUE_KEY).(string)
+}
+func TokenInfo(ctx context.Context) *Token {
+	return ctx.Value(CONTEXT_TOKEN_VALUE_KEY).(*Token)
+}
+func LoginId[T longIdType](ctx context.Context) T {
 	var tmp T
 	switch ctx.Value(CONTEXT_LOGIN_ID_KEY).(type) {
 	case T:
@@ -74,19 +94,19 @@ func GetLoginId[T longIdType](ctx context.Context) T {
 }
 
 type TokenHandler interface {
-	SaveToken(key string, token *Token) error
-	GetToken(key string) (*Token, error)
+	StoreToken(tokenValue string, token *Token) error
+	LoadToken(tokenValue string) (*Token, error)
 }
 
 type defaultToken struct {
 	Token map[string]*Token
 }
 
-func (d *defaultToken) SaveToken(key string, token *Token) error {
+func (d *defaultToken) StoreToken(key string, token *Token) error {
 	d.Token[key] = token
 	return nil
 }
-func (d *defaultToken) GetToken(key string) (*Token, error) {
+func (d *defaultToken) LoadToken(key string) (*Token, error) {
 	v, ok := d.Token[key]
 	if !ok {
 		return nil, errors.New("no token")
