2d7a1aae42e0e4cd45f9d2d45e34a60fd31ebd71
a little <little@autre.cn>
2024-10-27 22:59:45 +0800

    调整websocket

--- web_socket.go
+++ web_socket.go
@@ -13,8 +13,9 @@ import (
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+	ReadBufferSize:    1024,
+	WriteBufferSize:   1024,
+	EnableCompression: true,
 	CheckOrigin: func(r *http.Request) bool {
 		return true // 允许所有来源，生产环境中应该更严格
 	},
