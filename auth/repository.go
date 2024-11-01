3793dcd91546800231e696d1386fb8ba2820a360
a little <little@autre.cn>
2024-11-01 23:40:52 +0800

    修复token校验

--- auth/repository.go
+++ auth/repository.go
@@ -21,30 +21,30 @@ func NewUserRepository(db ...string) UserRepository {
 type GormUser struct{}
 
 func (r *GormUser) Save(ctx context.Context, m *User) error {
-	return gowk.DB().Save(m).Error
+	return gowk.DB(ctx).Save(m).Error
 
 }
 func (r *GormUser) GetByKey(ctx context.Context, key string) (*User, error) {
 	var d User
-	tx := gowk.DB().Where("key = ?", key).First(&d)
+	tx := gowk.DB(ctx).Where("key = ?", key).First(&d)
 	return &d, tx.Error
 }
 func (r *GormUser) GetById(ctx context.Context, id int64) (*User, error) {
 	var d User
-	tx := gowk.DB().First(&d, id)
+	tx := gowk.DB(ctx).First(&d, id)
 	return &d, tx.Error
 }
 func (r *GormUser) GetByToken(ctx context.Context, token string) (*User, error) {
 	var d User
-	tx := gowk.DB().Where("token = ?", token).First(&d)
+	tx := gowk.DB(ctx).Where("token = ?", token).First(&d)
 	return &d, tx.Error
 }
 func (r *GormUser) GetByPhone(ctx context.Context, phone string) (*User, error) {
 	var d User
-	tx := gowk.DB().Where("phone = ?", phone).First(&d)
+	tx := gowk.DB(ctx).Where("phone = ?", phone).First(&d)
 	return &d, tx.Error
 }
 func (r *GormUser) UpdateToken(ctx context.Context, userId int64, token string) error {
-	tx := gowk.DB().Model(&User{}).Where("id = ?", userId).Updates(map[string]interface{}{"token": token, "last_login": gowk.Now()})
+	tx := gowk.DB(ctx).Model(&User{}).Where("id = ?", userId).Updates(map[string]interface{}{"token": token, "last_login": gowk.Now()})
 	return tx.Error
 }
