f853fef45e0b6129df76582503bf71411657f8a2
a little <little@autre.cn>
2025-04-02 22:29:03 +0800

    增加公共方法

--- handler.go
+++ handler.go
@@ -13,16 +13,67 @@ func (c *Handler[T]) Page() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var page PageModel[T]
 		if err := c.ShouldBind(&page); err != nil {
-			Panic(ERR_PARAM, err)
+			c.Error(err)
+			return
 		}
 		var t T
 		if err := c.ShouldBind(&t); err != nil {
-			Panic(ERR_PARAM, err)
+			c.Error(err)
+			return
 		}
 		service := NewService[T](c)
 		res, err := service.Page(&page, &t)
 		if err != nil {
-			Panic(ERR, err)
+			c.Error(err)
+			return
+		}
+		Success(c, res)
+	}
+}
+func (c *Handler[T]) Add() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var t T
+		if err := c.ShouldBind(&t); err != nil {
+			c.Error(err)
+			return
+		}
+		service := NewService[T](c)
+		err := service.Save(&t)
+		if err != nil {
+			c.Error(err)
+			return
+		}
+		Success(c, t)
+	}
+}
+func (c *Handler[T]) Update() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var t T
+		if err := c.ShouldBind(&t); err != nil {
+			c.Error(err)
+			return
+		}
+		service := NewService[T](c)
+		err := service.Update(&t)
+		if err != nil {
+			c.Error(err)
+			return
+		}
+		Success(c, t)
+	}
+}
+func (c *Handler[T]) One() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var t T
+		if err := c.ShouldBind(&t); err != nil {
+			c.Error(err)
+			return
+		}
+		service := NewService[T](c)
+		res, err := service.One(&t)
+		if err != nil {
+			c.Error(err)
+			return
 		}
 		Success(c, res)
 	}
